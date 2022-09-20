b19410153f6f1b1674d3dfc79501233a19b26ef6
Eyal Mazuz <mazuze@post.bgu.ac.il>
2022-09-20 15:35:24 +0300

    Changed QED reward to the new format and added a similarity based reward

--- MolGen/src/utils/reward_fn.py
+++ MolGen/src/utils/reward_fn.py
@@ -6,27 +6,27 @@ from typing import Callable, Optional, Union, List
 
 import chemprop
 from rdkit import Chem
+from rdkit.Chem.QED import qed
+from rdkit import DataStructs
 from rdkit import RDLogger
 RDLogger.DisableLog('rdApp.*')
 import torch
 from tqdm import tqdm
 
-from .metrics import calc_qed, calc_sas
-
 def get_reward_fn(reward_names: List[str], paths: List[str]=None, multipliers: List[str]=None, **kwargs):
     reward_fns = []
     for reward_name, path, mult in zip(reward_names, paths, multipliers):
-        if reward_name == 'QED':
-            reward_fn = QEDReward(**kwargs)
-        
-        elif reward_name == 'IC50':
-            reward_fn = IC50Reward(**kwargs)
-
-        elif reward_name == 'Anti Cancer':
+        if reward_name == 'Anti Cancer':
             reward_fn = ChempropReward(path, reward_name, multiplier=eval(mult))
 
         elif reward_name == 'LIDI':
             reward_fn = ChempropReward(path, reward_name, multiplier=eval(mult))
+
+        elif reward_name == 'Sim':
+            reward_fn = SimilarityReward(path, reward_name, multiplier=eval(mult))
+        
+        elif reward_name == 'QED':
+            reward_fn = QEDReward(reward_name, multiplier=eval(mult)
         
         reward_fns.append(reward_fn)
 
@@ -36,18 +36,32 @@ def get_reward_fn(reward_names: List[str], paths: List[str]=None, multipliers: L
         return MultiReward(reward_fns)
 
 class Reward(ABC):
-    def __init__(self, multiplier:  Optional[Callable[[float], float]]=None, **kwargs) -> None:
+    def __init__(self, name, multiplier:  Optional[Callable[[float], float]]=None, eval_: bool=False, **kwargs) -> None:
+        self.name = name
         self.multiplier = multiplier
+        self._eval = eval_
         
     @abstractmethod
     def __call__(self, smiles: str):
         raise NotImplementedError
 
-class MultiReward(Reward):
-    def __init__(self, reward_fns, eval_: bool=False) -> None:
-        self.reward_fns = reward_fns
+    @property
+    def eval(self):
+        return self._eval
 
-        self._eval = eval_
+    @eval.setter
+    def eval(self, val):
+        for fn in self.reward_fns:
+            if hasattr(fn, '_eval'):
+                fn.eval = val
+        self._eval = val
+
+    def __str__(self,):
+        return self.name
+
+class MultiReward(Reward):
+    def __init__(self, reward_fns) -> None:
+        self.reward_fns = reward_fns
 
     def __call__(self, smiles):
         rewards = OrderedDict()
@@ -61,28 +75,36 @@ class MultiReward(Reward):
 
         return rewards
 
-    @property
-    def eval(self):
-        return self._eval
-
-    @eval.setter
-    def eval(self, val):
-        for fn in self.reward_fns:
-            if hasattr(fn, '_eval'):
-                fn.eval = val
-        self._eval = val
-
     def __str__(self):
         return "MultiReward"
+
+class SimilarityReward(Reward):
+    def __init__(self, smiles, name, multiplier=None, **kwargs):
+        super().__init__(name=name, multiplier=multiplier, **kwargs)
+
+        self.name = name
+        self.smiles = smiles
+        self.fp = Chem.RDKFingerprint(Chem.MolFromSmiles(smiles))
+
+    def __call__(self, smiles: List[str]):
+        mols = [Chem.MolFromSmiles(s) for s in smiles]
+        fps = [Chem.RDKFingerprint(mol) for mol in mols if mol is not None]
+        sims = DataStructs.BulkTanimotoSimilarity(self.fp, fps)
+        
+        rewards = [sims[i] if mols[i] is not None else 0 for i in range(len(mols)]
+
+        if self.multiplier is not None and not self.eval:
+            preds = [self.multiplier(pred) for pred in preds]
+
+        return rewards
 
 class ChempropReward(Reward):
     def __init__(self,
                 predictor_path,
                 name: str='Chemprop',
                 multiplier: Optional[Callable[[float], float]]=None,
-                eval_: bool=False,
                 **kwargs) -> None:
-        super().__init__(multiplier=multiplier, **kwargs)
+        super().__init__(name=name, multiplier=multiplier, **kwargs)
         arguments = [
             '--test_path', '/dev/null',
             '--preds_path', '/dev/null',
@@ -94,9 +116,6 @@ class ChempropReward(Reward):
         self.args = chemprop.args.PredictArgs().parse_args(arguments)
 
         self.model_objects = chemprop.train.load_model(args=self.args)
-
-        self._eval = eval_
-        self.name = name
 
     def __call__(self, smiles: str) -> float:
         if isinstance(smiles, list):
@@ -124,116 +143,25 @@ class ChempropReward(Reward):
 
         return preds
 
-    @property
-    def eval(self):
-        return self._eval
-
-    @eval.setter
-    def eval(self, val):
-        self._eval = val
-
-    def __str__(self,):
-        return self.name
-
-class IC50Reward(Reward):
-    def __init__(self,
-                predictor_path,
-                tokenizer,
-                multiplier: Optional[Callable[[float], float]]=lambda x: math.exp(x / 3),
-                **kwargs) -> None:
-
-        super().__init__(multiplier)
-        self.predictor = torch.load(predictor_path)
-        self.predictor = self.predictor.eval()
-
-        self.tokenizer = tokenizer
-
-    def __call__(self, smiles: str):
-        if isinstance(smiles, str):
-            smiles = [smiles]
-        
-        rewards = []
-        for s in tqdm(smiles):
-            try:
-                mol = Chem.MolFromSmiles(s)
-                if mol is None:
-                    return 0
-
-                encodings = self.tokenizer('[CLS]' + s, padding=False)
-
-                for k, v in encodings.items():
-                    encodings[k] = torch.tensor([v]).long().to(self.predictor.device)
-                with torch.no_grad():
-                    reward = self.predictor(**encodings)
-                if self.multiplier is not None:
-                    reward = self.multiplier(reward)
-                if isinstance(reward, torch.Tensor):
-                    reward = reward.cpu().item()
-
-            except Exception as e:
-                # print(f'Failed at: {smiles}')
-                reward = 0 
-
-        return rewards
-
-    def __str__(self,):
-        return "IC50"
-        
 class QEDReward(Reward):
     def __init__(self,
-                 multiplier: Optional[Callable[[float], float]]=lambda x: x * 10,
-                 negative_reward: float=0,
+                 name,
+                 multiplier=None,
                  **kwargs):
 
-        super(QEDReward, self).__init__(multiplier)
+        super(QEDReward, self).__init__(name=name, multiplier=multiplier)
         self.negative_reward = negative_reward
-        self.reward_fn = calc_qed
 
     def __call__(self, smiles: str):
         if isinstance(smiles, str):
             smiles = [smiles]
         
-        rewards = []
-        for smile in tqdm(smiles):
-            try:
-                mol = Chem.MolFromSmiles(smile)
-                reward = self.reward_fn(mol)
-                if self.multiplier:
-                    reward = self.multiplier(reward)
-            
-            except Exception as e:
-                reward = self.negative_reward
-
-            rewards.append(reward) 
+        rewards = [qed(Chem.MolFromSmiles(s) if Chem.MolFromSmiles(s) is not None else 0 for s in smiles]
         
+        if self.multiplier is not None and not self.eval:
+            rewards = [self.multiplier(pred) for pred in preds]
+            
         return rewards
-
-    def __str__(self,):
-        return "QED"
-
-def penalized_qed_reward(smiles: Union[List[str], str], fn: Callable[[float], float]=lambda x: x*10) -> float:
-    mol = Chem.MolFromSmiles(smiles)
-    if mol:
-        qed = calc_qed(mol)
-        ring_info = mol.GetRingInfo()
-        rings = ring_info.AtomRings()
-
-        reward = fn(qed) + 5 * len(rings)# - sum(map(len, rings))
-    else:
-        reward = 0
-
-    
-    return reward
-
-def sas_reward(smiles: str, fn: Callable[[float], float]=lambda x: -math.exp(x/3)) -> float:
-    mol = Chem.MolFromSmiles(smiles)
-    if mol:
-        sas = calc_sas(mol)
-        reward = fn(sas)
-    else:
-        reward = -1000
-    
-    return reward
 
 def main():
     with QEDReward() as rfn:
