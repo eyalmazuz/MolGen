9a7782de3669170cf5adb81faccc683b90761069
Eyal Mazuz <mazuz.eyal@gmail.com>
2024-03-12 19:07:24 +0200

    Added initial mutli rewards tests, changed some text in tests for more clarity

--- tests/molgen/rewards/test_reward_factory.py
+++ tests/molgen/rewards/test_reward_factory.py
@@ -2,7 +2,6 @@ from molgen.rewards.reward_factory import get_rewards
 from molgen.rewards.functions.rdkit_rewards import QEDReward, PenalizedLogPReward
 from molgen.rewards.functions.multi_reward import MultiReward
 
-
 def test_get_single_reward_empty_dict():
     rewards_dict = {
         "QED": {}
@@ -13,6 +12,8 @@ def test_get_single_reward_empty_dict():
 
     qed = reward("CCC")
     assert reward.scale is None
+    assert abs(round(qed, 4) - 0.3854) < 0.0001
+
 
 def test_get_single_reward_scale_in_dict():
     rewards_dict = {
@@ -22,6 +23,9 @@ def test_get_single_reward_scale_in_dict():
     reward = get_rewards(rewards_dict)
     assert isinstance(reward, QEDReward)
     assert reward.scale is not None
+
+    qed = reward("CCC")
+    assert abs(round(qed, 4) - 3.854) < 0.001
 
 
 def test_get_multiple_rewards_empty_list():
