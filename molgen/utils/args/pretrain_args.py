36c30da0db7c3f246cca22cbf99cd3b210c29d4f
orgindes <gindesor@gmail.com>
2024-06-29 18:07:45 +0300

    replace options with choices in the args

--- molgen/utils/args/pretrain_args.py
+++ molgen/utils/args/pretrain_args.py
@@ -8,8 +8,8 @@ def get_pretrain_args() -> argparse.Namespace:
     parser.add_argument("--data_path", type=str, required=True, help="Path to the training data")
     parser.add_argument("--tokenizer_path", type=str, required=True, help="Path to the tokenizer used for training")
     parser.add_argument("--checkpoint_dir", type=str, required=True, help="Path to save the model")
-    parser.add_argument("--model_type", type=str, required=True, options=["GPT", "DT"], help="Type of model to use for training")
-    parser.add_argument("--dataset_type", type=str, required=True, options=["SMILES", "DT_SMILES"], help="Type of dataset to use for training")
+    parser.add_argument("--model_type", type=str, required=True, choices=["GPT", "DT"], help="Type of model to use for training")
+    parser.add_argument("--dataset_type", type=str, required=True, choices=["SMILES", "DT_SMILES"], help="Type of dataset to use for training")
     parser.add_argument("--config_path", type=str, required=True, help="Path to the connfig containing training and model params")
 
     return parser.parse_args()
