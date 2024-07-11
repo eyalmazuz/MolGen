- expand the attention mask to (block_size, seq_len) for mean_pooling - fix mean_pooling to the correct shapes

molgen/models/dt_gpt.py
@@ -163,7 +163,7 @@ class DtGPT(nn.Module):
 
         self.state_embedding = self.tok_emb
         # self.state_encoder = nn.Linear(config.block_size // 3 * config.n_embd, config.n_embd)
-        self.ret_emb = nn.Sequential(nn.Linear(1, config.n_embd), nn.Tanh())
+        self.rtg_emb = nn.Sequential(nn.Linear(1, config.n_embd), nn.Tanh())
 
         self.action_embeddings = self.tok_emb   # Actions are simply SMILES tokens to add to the state
         nn.init.normal_(self.action_embeddings.weight, mean=0.0, std=0.02)
@@ -236,9 +236,11 @@ class DtGPT(nn.Module):
 
     @staticmethod
     def mean_pooling(model_output, attention_mask):
-        token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(model_output.size()).float()
+        return torch.sum(model_output * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        # token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
+        # input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        # return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     # state, action, and return
     def forward(self, states, actions, targets=None, rtgs=None, attention_mask=None):
@@ -256,12 +258,11 @@ class DtGPT(nn.Module):
         assert block_size <= self.block_size, \
             f"Cannot forward sequence of length {block_size}, block size is only {self.block_size}"
         state_embeddings = self.state_embedding(states)  # (batch_size, block_size, state_size, n_embd)
-        state_embeddings = self.mean_pooling(state_embeddings, attention_mask)
-        # state_embeddings = state_embeddings.reshape(batch_size, block_size, -1).contiguous()
-        # state_embeddings = self.state_encoder(state_embeddings)  # (batch_size, block_size, n_embd)
+        # TODO: replace mean_pooling with a mini-transformer model
+        state_embeddings = self.mean_pooling(state_embeddings, attention_mask)  # (batch_size, block_size, n_embd)
 
         if actions is not None and self.model_type == 'reward_conditioned':
-            rtg_embeddings = self.ret_emb(rtgs.unsqueeze(-1))  # (batch, block_size, n_embd)
+            rtg_embeddings = self.rtg_emb(rtgs.unsqueeze(-1))  # (batch, block_size, n_embd)
             action_embeddings = self.action_embeddings(actions)  # (batch, block_size, n_embd)
 
             token_embeddings = torch.zeros(