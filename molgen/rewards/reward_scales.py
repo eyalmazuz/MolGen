c50588ef95208399321bda6d77cb81c0e8948593
OrGindes <gindesor@gmail.com>
2024-10-02 17:19:33 +0300

    - add new configs for multi rewards with char tokenizer - add minmax scaling for rewards

--- molgen/rewards/reward_scales.py
+++ molgen/rewards/reward_scales.py
@@ -1,2 +1,13 @@
 def mult(value: float, factor: float=10.) -> float:
     return factor * value
+
+
+def minmax(value: float, min_value: float, max_value: float, new_min: float = 0.0, new_max: float = 1.0) -> float:
+    if max_value == min_value:
+        return new_min
+    if value < min_value:
+        return new_min
+    if value > max_value:
+        return new_max
+
+    return new_min + ((value - min_value) / (max_value - min_value)) * (new_max - new_min)
