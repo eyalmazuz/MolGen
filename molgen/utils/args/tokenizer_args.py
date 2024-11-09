543e7ddd802b70e293568d47f34689e4975e4c4a
OrGindes <gindesor@gmail.com>
2024-11-09 22:19:38 +0200

    - support SELFIES

--- molgen/utils/args/tokenizer_args.py
+++ molgen/utils/args/tokenizer_args.py
@@ -5,6 +5,7 @@ def get_tokenizer_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--type", type=str, choices=["Char", "BPE"], help="Type of tokenizer to train")
+    parser.add_argument("--string_type", type=str, choices=["SMILES", "SELFIES"], default="SMILES", help="Input string type")
     parser.add_argument("--vocab_size", type=int, help="Vocab size for tokenizer of type BPE")
     parser.add_argument("--save_path", type=str, required=True, help="Where to save the trained tokenizer")
     parser.add_argument("--data_path", type=str, nargs="+", required=True, help="Where to save the trained tokenizer")
