bee0664545fda8d0a5347fa7371ec21632ecbb11
OrGindes <gindesor@gmail.com>
2025-01-21 14:13:17 +0200

    - goal-conditioned with older train_dt.py functions

--- molgen/training/train_dt.py
+++ molgen/training/train_dt.py
@@ -6,6 +6,7 @@ import numpy as np
 from tqdm import tqdm
 from typing import Any
 
+import wandb
 import torch
 import selfies as sf
 
@@ -15,7 +16,7 @@ from molgen.utils.plot_utils import save_plot
 
 class Trainer:
 
-    def __init__(self, model, train_dataset, test_dataset, reward_func, config, wandb_run=None):
+    def __init__(self, model, train_dataset, test_dataset, reward_func, config, device: str = "cuda", wandb_log=False):
         self.model = model
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
@@ -25,13 +26,14 @@ class Trainer:
         self.eos_token_id = train_dataset.dataset.tokenizer.eos_token_id
         self.pad_token_id = train_dataset.dataset.tokenizer.pad_token_id
         self.ignore_token_id = train_dataset.collate_fn.ignore_index
-        self.wandb_run = wandb_run
+        self.wandb_log = wandb_log
+        self.device = device
         self.optimizer = None
 
         # take over whatever gpus are on the system
-        if torch.cuda.is_available():
-            self.device = torch.cuda.current_device()
-            self.model = torch.nn.DataParallel(self.model).to(self.device)
+        # if torch.cuda.is_available():
+        #     self.device = torch.cuda.current_device()
+        #     self.model = torch.nn.DataParallel(self.model).to(self.device)
 
     def save_checkpoint(self, epoch):
         ckpt_name = f"epoch_{epoch}.pth" if self.test_dataset is None else f"best.pth"
@@ -45,10 +47,6 @@ class Trainer:
         torch.save(checkpoint, os.path.join(self.config.get("ckpt_path", "."), ckpt_name))
 
     def load_checkpoint(self, ckpt_name="latest"):
-        if not self.config.get("load_ckpt"):
-            print(f"Load_Checkpoint set to False! starting training from scratch")
-            return 0, 0
-
         checkpoint_dir = self.config.get("ckpt_path", ".")
         if ckpt_name == "latest":
             checkpoint_files = [f for f in os.listdir(checkpoint_dir) if f.startswith("epoch_") and f.endswith(".pth")]
@@ -62,7 +60,7 @@ class Trainer:
                 for file in checkpoint_files
                 if re.search(r"epoch_(\d+)", file)
             ]
-            ckpt_name = f"epoch_{max(epoch_numbers)}.pth"
+            ckpt_name =  f"epoch_{max(epoch_numbers)}.pth"
 
         path = os.path.join(checkpoint_dir, ckpt_name)
         checkpoint = torch.load(path)
@@ -96,13 +94,12 @@ class Trainer:
                     batch = {k: v.to(self.device) for k, v in batch.items()}    # place data on the correct device
                 x = batch["input_ids"]  # states
                 y = batch["labels"]     # actions
-                r = batch["rtg"]        # rtgs (reward-to-go)
+                r = batch["rtgs"]       # rtgs (reward-to-go)
                 a = batch["attention_mask"]
-                g = batch.get("goal", None)
 
                 # forward the model
                 with torch.set_grad_enabled(is_train):
-                    logits, loss = model(input_ids=x, labels=y, targets=y, rtgs=r, attention_mask=a, goal=g)
+                    logits, loss = model(input_ids=x, labels=y, targets=y, rtgs=r, attention_mask=a)
                     # logits, loss = model(x, y, y, r, t)
                     loss = loss.mean()  # collapse all losses if they are scattered on multiple gpus
                     total_loss += loss
@@ -143,8 +140,13 @@ class Trainer:
             # if not is_train:
             episode_loss = total_loss.item() / len(loader)
             print(f"\nMean Epoch Loss: {episode_loss:.4f}")
-            if self.wandb_run:
-                self.wandb_run.log({'training_loss': episode_loss, 'epoch': epoch})
+            if self.wandb_log:
+                wandb.log(
+                    {
+                        'training_loss': episode_loss,
+                        'epoch': epoch
+                    }
+                )
 
             return episode_loss
 
@@ -169,12 +171,12 @@ class Trainer:
                 self.save_checkpoint(epoch)
 
             # -- pass in target returns
-            model_type = self.model.module.model_type if hasattr(self.model, "module") else self.model.model_type
-            if model_type == 'naive':
-                eval_return = self.get_returns(0)
-            elif model_type == 'reward_conditioned':
-                # TODO: return should be based on the reward function, for now put 1 for a scaled reward
-                eval_return = self.get_returns(1)
+            # model_type = self.model.module.model_type if hasattr(self.model, "module") else self.model.model_type
+            # if model_type == 'naive':
+            #     eval_return = self.get_returns(0)
+            # elif model_type == 'reward_conditioned':
+            #     # TODO: return should be based on the reward function, for now put 1 for a scaled reward
+            #     eval_return = self.get_returns(1)
 
         if self.wandb_run is None:
             [print(f"{ep_loss:.5f}") for ep_loss in epoch_losses]  # Debug print
@@ -190,8 +192,6 @@ class Trainer:
             init_state = torch.tensor([self.bos_token_id], dtype=torch.int64)
             init_state = init_state.to(self.device).unsqueeze(0).unsqueeze(0)
             rtgs = [ret]
-            goal_idx = np.random.choice(self.model.module.config.n_goals)
-            goal = [goal_idx]
             # first state is from env, first rtg is target return, and first timestep is 0
             sampled_action = sample(
                 model=self.model,
@@ -201,7 +201,6 @@ class Trainer:
                 sample=True,
                 actions=None,
                 rtgs=torch.tensor(rtgs, dtype=torch.float32).to(self.device).unsqueeze(0).unsqueeze(-1),
-                goal=torch.tensor(goal, dtype=torch.int64).to(self.device).unsqueeze(0),
                 # timesteps=torch.zeros((1, 1, 1), dtype=torch.int64).to(self.device)
             )
 
@@ -217,13 +216,13 @@ class Trainer:
                 sequence = self.train_dataset.dataset.tokenizer.decode(state, skip_special_tokens=True)[0]
                 if self.train_dataset.dataset.string_type == "SELFIES":
                     sequence = sf.decoder(sequence)
-                reward = self.reward_func[goal_idx](sequence)
+                reward = self.reward_func(sequence)
                 done = action == self.eos_token_id  # mol is complete when [EOS] token is generated
                 reward_sum = reward
                 j += 1
 
                 # if molecule length exceeds block_size and [EOS] token wasn't generated terminate generation
-                if len(state) >= self.model.module.block_size // 3 and not done:
+                if len(state) >= self.model.config.max_seq_len and not done:
                     terminated = True
 
                 if done or terminated:
@@ -236,7 +235,6 @@ class Trainer:
                 all_states = torch.cat([all_states, tensor_state], dim=1)
 
                 rtgs += [rtgs[-1] - reward]
-                goal.append(goal_idx)
                 # all_states has all previous states and rtgs has all previous rtgs (will be cut to block_size in utils.sample)
                 # timestep is just current timestep
                 sampled_action = sample(
@@ -247,8 +245,7 @@ class Trainer:
                     sample=True,
                     actions=torch.tensor(actions, dtype=torch.long).to(self.device).unsqueeze(0),
                     rtgs=torch.tensor(rtgs, dtype=torch.float32).to(self.device).unsqueeze(0),
-                    attention=torch.tensor(np.tril(np.ones(all_states.shape[1:])), dtype=torch.long).to(self.device).unsqueeze(0),
-                    goal=torch.tensor(goal, dtype=torch.int64).to(self.device).unsqueeze(0),
+                    attention=torch.tensor(np.tril(np.ones(all_states.shape[1:])), dtype=torch.long).to(self.device).unsqueeze(0)
                     # timesteps=(min(j, self.config.max_timestep) * torch.ones((1, 1, 1), dtype=torch.int64).to(self.device)))
                 )
         eval_return = sum(T_rewards) / 10.
@@ -266,7 +263,8 @@ def run_dt_training(
         reward_func,
         train_config,
         test_dataloader=None,
-        wandb_run=None,
+        device: str = "cuda",
+        wandb_log=False,
 ):
-    trainer = Trainer(model, train_dataloader, test_dataloader, reward_func, train_config, wandb_run)
+    trainer = Trainer(model, train_dataloader, test_dataloader, reward_func, train_config, device, wandb_log)
     trainer.train(optimizer)
