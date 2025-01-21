340439267e2757e009b4d5932489f00bb1a5d130
OrGindes <gindesor@gmail.com>
2025-01-21 15:22:33 +0200

    - test direct import

--- molgen/rewards/functions/rdkit_rewards.py
+++ molgen/rewards/functions/rdkit_rewards.py
@@ -7,8 +7,8 @@ from rdkit.Chem.Crippen import MolLogP  # type: ignore
 from rdkit.Chem.QED import qed
 from rdkit.Chem.rdchem import Mol
 from rdkit.RDConfig import RDContribDir
-sys.path.append(os.path.join(RDContribDir, 'SA_Score'))
-import sascorer
+from rdkit.Contrib.SA_Score import sascorer
+
 
 from molgen.rewards.reward import AbstractReward, RewardScale
 
