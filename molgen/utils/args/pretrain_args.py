e3cd3500293689a935962cba378e9338f84f30ce
OrGindes <gindesor@gmail.com>
2025-02-23 22:14:21 +0200

    - update input args to match v2 args

--- molgen/utils/args/pretrain_args.py
+++ molgen/utils/args/pretrain_args.py
@@ -5,18 +5,20 @@ def get_pretrain_args() -> argparse.Namespace:
 
     parser: argparse.ArgumentParser = argparse.ArgumentParser(allow_abbrev=False)
 
-    parser.add_argument("--data_path", type=str, required=True, help="Path to the training data")
-    parser.add_argument("--tokenizer_path", type=str, required=True, help="Path to the tokenizer used for training")
-    parser.add_argument("--checkpoint_dir", type=str, required=True, help="Path to save the model")
-    parser.add_argument("--model_type", type=str, required=True, choices=["GPT", "DT"], help="Type of model to use for training")
-    parser.add_argument("--dataset_type", type=str, required=True, choices=["SMILES", "DT_SMILES", "SELFIES", "DT_SELFIES"], help="Type of dataset to use for training")
-    parser.add_argument("--config_path", type=str, required=True, help="Path to the connfig containing training and model params")
+    parser.add_argument("--data-path", type=str, required=True, help="Path to the training data")
+    parser.add_argument("--tokenizer-path", type=str, required=True, help="Path to the tokenizer used for training")
+    parser.add_argument("--save-path", type=str, required=True, help="Path to save the model")
+    parser.add_argument("--model-type", type=str, required=True, choices=["GPT", "DT"], help="Type of model to use for training")
+    parser.add_argument("--dataset-type", type=str, required=True, choices=["SMILES", "DT_SMILES", "SELFIES", "DT_SELFIES"], help="Type of dataset to use for training")
+    parser.add_argument("--config-path", type=str, required=True, help="Path to the connfig containing training and model params")
 
     # Wandb parameters to log results
-    parser.add_argument('--wandb_key', type=str, help='wandb api key for user login', default=None)
-    parser.add_argument('--wandb_proj', type=str, default='DecisionMol',
+    parser.add_argument('--wandb-key', type=str, help='wandb api key for user login', default=None)
+    parser.add_argument('--wandb-proj', type=str, default='DecisionMol',
                         help='name of wandb project to upload results')
-    parser.add_argument('--wandb_entity', type=str, default='bgu-sise',
+    parser.add_argument('--wandb-entity', type=str, default='bgu-sise',
                         help='wandb entity associated with the project')
+    parser.add_argument('--wandb-name', type=str, help='wandb run name', default=None)
+
 
     return parser.parse_args()
