fc717d07f63525956f2621ca33d9733d2990d33d
OrGindes <gindesor@gmail.com>
2025-02-25 18:22:08 +0200

    - cProfile for debugging

--- molgen/models/dt_gpt.py
+++ molgen/models/dt_gpt.py
@@ -165,10 +165,6 @@ class DtGPT(nn.Module):
 
         self.block_size = config.block_size
         self.apply(self._init_weights)
-        # apply special scaled init to the residual projections, per GPT-2 paper
-        for pn, p in self.named_parameters():
-            if pn.endswith("c_proj.weight"):
-                torch.nn.init.normal_(p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer))
 
         logger.info("number of parameters: %e", sum(p.numel() for p in self.parameters()))
 
