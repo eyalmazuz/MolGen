d8bb9e9b01c74b994886757d6096107ee400557e
evronba <evronba@ise-pheno-01.auth.ad.bgu.ac.il>
2026-06-06 12:33:51 +0300

    Conditional Checkpointing based on Best Validation Loss and config updates

--- molgen/training/train_dt.py
+++ molgen/training/train_dt.py
@@ -200,11 +200,11 @@ class Trainer:
                 test_loss = run_epoch('test')
 
             # supports early stopping based on the test loss, or save every X epochs if no test set
-            good_model = (epoch > 2 and (self.test_dataset is None and (epoch % 1 == 0))) or test_loss < best_loss
+            # good_model = (epoch > 2 and (self.test_dataset is None and (epoch % 1 == 0))) or test_loss < best_loss   #always true if no test set, otherwise only true if test loss improves
+            good_model = (epoch > 2 and (self.test_dataset is None and (epoch % 1 == 0))) and test_loss < best_loss
             if self.save_path is not None and good_model:
                 best_loss = test_loss
-                self.save_checkpoint(epoch)
-
+                self.save_checkpoint(epoch) # ALWAYS saves with name "epoch_{epoch}.pth"
             # self.save_checkpoint(epoch, ckpt_name="latest.pth")
 
             # -- pass in target returns
